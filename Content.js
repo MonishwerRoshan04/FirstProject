7f9fc91038808884ee1562f2d0e64c34e0b484fc
B.N.MonishwerRoshan <monishwermona@gmail.com>
2025-10-16 06:13:49 +0530

    Update Content.js

--- Content.js
+++ Content.js
@@ -6,9 +6,45 @@ const Content = () => {
         let randomNames = Math.floor(Math.random() * names.length);
         return names[randomNames];
       }
+      // let handleClick = () => {
+      //   console.log("Thankyou Guys");
+      // }
+
+      // let handleClick2 = () => {
+      //   console.log("Thankyou Guys");
+      // }
+
+      // let handleClick3 = (name) => {
+      //   console.log(`Thankyou ${name}`);
+      // }
+
+      // let handleClick4 = (e) => {
+      //   console.log(e.target);
+      // }
+
+      // let handleClick5 = (e) => {
+      //   console.log(e.target);
+      // }
+
+      // let handleClick6 = (e) => {
+      //   console.log(e.target);
+      // }
+
+       let handleClick5 = (e) => {
+        console.log(e.target.innerText);
+      }
 
   return (
+    <div>    
     <p>Lets {handleNameChange()} Money</p>
+    {/* <button onClick={handleClick}>Subscribe</button> */}
+    {/* <button onClick={handleClick2()}>Subscribe</button> */}
+    {/* <button onClick={() => handleClick3("Vijay")}>Subscribe</button> */}
+    {/* <button onClick={(e) => handleClick4(e)}>Subscribe</button> */}
+    {/* <button onClick={() => handleClick5(e)}>Subscribe</button> */}
+    {/* <button onClick={handleClick6}>Subscribe</button> */}
+    <button onClick={(e) => handleClick5(e)}>Subscribe</button>
+    </div>
   )
 }
 
