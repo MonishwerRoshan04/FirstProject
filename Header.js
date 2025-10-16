93cb2f7a5370140b4e89dcdffe1e843a018da563
B.N.MonishwerRoshan <monishwermona@gmail.com>
2025-10-16 06:13:25 +0530

    Update Header.js

--- Header.js
+++ Header.js
@@ -1,5 +1,22 @@
 import React from 'react'
 
+// const Header = () => {
+//   let colors = {backgroundColor:"red",color:"#fff"}
+//   return (
+//     <header style={colors}>
+//         <h1>To Do list</h1>
+//     </header>
+//   )
+// }
+
+// const Header = () => {  
+//   return (
+//     <header style={{backgroundColor:"red",color:"#fff"}}>
+//         <h1>To Do list</h1>
+//     </header>
+//   )
+// }
+
 const Header = () => {
   return (
     <header>
